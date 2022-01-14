a83621e4f1aa45d6778662f751703f1652fb5320
Ivan Urra <ivan.urra.wd@gmail.com>
2022-01-14 07:59:17 +0100

    start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-// YOU NEED A API KEY FROM GIPHY DEV WEB
-const apiCall = fetch (`http://api.giphy.com/v1/gifs/random?api_key=${process.env.REACT_APP_KEY}`);
+import React from "react";
+import ReactDOM from "react-dom";
+import GifExpertApp from "./components/GifExpertApp";
+import "./index.css";
 
-apiCall
-  .then (res => res.json())
-  .then (({data}) => {
-    const {url} = data.images.original;
-    const img = document.createElement('img');
-    img.src = url;
-    document.body.append(img);
-  })
-  .catch (console.warn)
+ReactDOM.render(<GifExpertApp />, document.getElementById("root"));
