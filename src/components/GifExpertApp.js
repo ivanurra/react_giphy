c57a11cabf89234e68818827995b7f5e865f5138
Ivan Urra <ivan.urra.wd@gmail.com>
2022-01-19 08:02:51 +0100

    setState

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["item1", "item2", "item3"]);
+
+  const handleAdd = () => {
+    setCategories([...categories, "item4"]);
+  };
+
   return (
-    <div>
+    <>
       <h2>GifExpertApp</h2>
       <hr />
-    </div>
+      <button onClick={handleAdd}>Add</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+    </>
   );
 };
 
