4313345ec0ee7b2b7bb22c995dad3dc7baaf4aaa
Ivan Urra <ivan.urra.wd@gmail.com>
2022-01-21 17:50:02 +0100

    useState initial

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export const AddCategory = ({ setCategories }) => {
-  const [inputValue, setInputValue] = useState();
+  const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
