be4bba7ed900b3b75ae527a5be123df78e626eee
Ivan Urra <ivan.urra.wd@gmail.com>
2022-01-21 08:10:36 +0100

    handleSubmit

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -3,9 +3,18 @@ import React, { useState } from "react";
 export const AddCategory = () => {
   const [inputValue, setInputValue] = useState("Search");
 
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submit done!😉");
+  };
+
   return (
-    <>
-      <input type="text" value={inputValue} />
-    </>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </form>
   );
 };
