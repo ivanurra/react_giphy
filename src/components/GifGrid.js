8ea0d4fe000967633ec74ad10fa43893590b2aa9
Ivan Urra <ivan.urra.wd@gmail.com>
2022-01-25 07:57:05 +0100

    fetchAPI

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,24 @@
 import React from "react";
 
 export const GifGrid = ({ category }) => {
+  const getGifs = async () => {
+    const url = `https://api.giphy.com/v1/gifs/search?limit=10&q=Cats&api_key=${process.env.REACT_APP_API_KEY}`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map (img => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url
+      }
+    })
+
+    console.log(gifs);
+  };
+
+  getGifs();
+
   return (
     <div>
       <h3>{category}</h3>
