46a4bda7136d82098a7c08f35f42e635c464fb10
Ivan Urra <ivan.urra.wd@gmail.com>
2022-01-26 08:41:22 +0100

    useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const GifGrid = ({ category }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    getGifs();
+  }, []);
+
   const getGifs = async () => {
     const url = `https://api.giphy.com/v1/gifs/search?limit=10&q=Cats&api_key=${process.env.REACT_APP_API_KEY}`;
     const resp = await fetch(url);
-    const {data} = await resp.json();
+    const { data } = await resp.json();
 
-    const gifs = data.map (img => {
+    const gifs = data.map((img) => {
       return {
         id: img.id,
         title: img.title,
-        url: img.images?.downsized_medium.url
-      }
-    })
+        url: img.images?.downsized_medium.url,
+      };
+    });
 
     console.log(gifs);
   };
-
-  getGifs();
 
   return (
     <div>
