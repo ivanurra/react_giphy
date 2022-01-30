7385d30078509b9f58da54bde3b7aae967af247e
Ivan Urra <ivan.urra.wd@gmail.com>
2022-01-30 13:13:48 +0100

    css added

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,9 +3,9 @@ import React from "react";
 export const GifGridItem = ({ title, url }) => {
 
   return (
-    <div>
-      <h3>{title}</h3>
-      <img src={url} alt={title} height="300px"/>
+    <div className="card">
+      <p>{title}</p>
+      <img src={url} alt={title}/>
     </div>
   );
 };
